c8ad9ff8d95b3d9b1b5f09e66b2cf00ad8e30822
nelliott82 <nikkoelliott@gmail.com>
2022-06-23 18:24:54 -0400

    Added Total component

---
+++ client/src/components/Total.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 50%;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 5px;
+  grid-template-rows: 0.1fr;
+  justify-items: center;
+  align-items: center;
+`
+const Total = styled.div`
+  grid-column: 1;
+  grid-row: 1;
+`
+
+var TotalComponent = ({ total }) => {
+
+return (
+    <Container>
+        <Total>
+          <span>Round Total: {total}</span>
+        </Total>
+    </Container>
+  )
+}
+
+export default TotalComponent;
