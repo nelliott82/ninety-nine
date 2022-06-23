fd953329b195095ad2e9499445f08bc164bfb503
nelliott82 <nikkoelliott@gmail.com>
2022-06-23 16:00:47 -0400

    Updated styling

--- client/src/components/PlayerOne.jsx
+++ client/src/components/PlayerOne.jsx
@@ -5,9 +5,9 @@ const PlayerOneArea = styled.div`
   width: 100%;
   height: 50%;
   display: grid;
-  display: grid;
-  grid-template-columns: 2fr 1fr 1fr 1fr 2fr;
-  grid-template-rows: 0.2fr 2fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  gap: 5px;
+  grid-template-rows: 0.1fr 2fr;
   justify-items: center;
   align-items: center;
 `
