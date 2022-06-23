6521941fc254d4fdba9fbd40b570fed4171b07b4
nelliott82 <nikkoelliott@gmail.com>
2022-06-23 14:54:38 -0400

    MVP styling done for now

--- client/src/components/Computer.jsx
+++ client/src/components/Computer.jsx
@@ -1,30 +1,76 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const ComputerArea = styled.div`
+  width: 100%;
+  height: 50%;
+  display: grid;
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr 1fr 2fr;
+  gap: 10px;
+  grid-template-rows: 0.2fr 2fr 0.5fr;
+  justify-items: center;
+  align-items: center;
+`
+
+const Name = styled.div`
+  grid-column: 2;
+  grid-row: 1;
+`
+const Strikes = styled.div`
+  grid-column: 4;
+  grid-row: 1;
+`
+
+const Holder = styled.div`
+  grid-column: ${({index}) => index};
+  grid-row: 2;
+  width: 130px;
+  height: 200px;
+  border: 2px solid black;
+`
+
 const ComputerCards = styled.img`
-  width: 10%;
-  margin: 0 7px;
+  width: 130px;
+  height: 195px;
+  grid-column: ${({index}) => index};
+  grid-row: 2;
 `
 ComputerCards.defaultProps = {
   src: ''
 }
 
+const Thinking = styled.div`
+  grid-column: 3;
+  grid-row: 3;
+`
+
+const FinalMsg = styled.div`
+  grid-column: 3;
+  grid-row: 3;
+`
+
+var placeHolder = [2, 3, 4];
+
 var ComputerComponent = ({ strikes, computerHand, thinking, over }) => {
 
 return (
-    <div>
-      <div>Computer:</div>
-      <div>Strikes: {strikes[1]}</div>
-        {computerHand.length ? computerHand.map(card => <ComputerCards key={card[0] + 'c'}
-                                                                       src='/assets/cards/back.jpg' />)
-                                                        : null}
-        {thinking ? <div>Thinking...</div> : null}
+    <ComputerArea>
+      <Name>Computer:</Name>
+      <Strikes>Strikes: {strikes[1]}</Strikes>
+        {computerHand.length ? computerHand.map((card, i) => <Holder key={card[0] + 'c'} index={i + 2}>
+                                                                <ComputerCards key={card[0] + 'c'}
+                                                                               index={i + 2}
+                                                                               src='/assets/cards/back.jpg' />
+                                                             </Holder>)
+                             : placeHolder.map(holder => <Holder key={holder} index={holder} />)}
+        {thinking ? <Thinking>Thinking...</Thinking> : null}
         {over ? strikes[0] === 2 ?
-              <div>"Better luck next time!"</div>
+              <FinalMsg>"Better luck next time!"</FinalMsg>
               :
-              <div>"I'm not feeling well. That's the only reason you won."</div>
+              <FinalMsg>"Nooooooo!!"</FinalMsg>
         : null}
-    </div>
+    </ComputerArea>
   )
 }
 
