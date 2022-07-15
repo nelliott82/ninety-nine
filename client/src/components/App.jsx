139c49f3478b011544681ddd34bcdbb7ebefe132
nelliott82 <nikkoelliott@gmail.com>
2022-07-15 18:24:26 -0400

    Added grid rows and columns and positions of items to start modal

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -66,19 +66,33 @@ const StartModal = styled.div`
 
 const StartContainer = styled.div`
   position: absolute;
+  background-color: white;
   display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  justify-items: center;
+  align-items: center;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
 `;
 
+const BotsDropDownLabel = styled.div`
+  grid-row: 1;
+  width: 5rem;
+  height: 2rem;
+  font-size: 1.5em;
+`;
+
 const BotsDropDown = styled.select`
+  grid-row: 2;
   width: 5rem;
   height: 2rem;
   font-size: 1.5em;
 `;
 
 const StartButton = styled.button`
+  grid-row: 3;
   width: 10rem;
   height: 4rem;
   font-size: 1.5em;
@@ -319,7 +333,7 @@ var App = () => {
     <GlobalStyle/>
     <StartModal started={started} >
       <StartContainer>
-        <div>Select number of computer opponents: </div>
+        <BotsDropDownLabel>Select number of computer opponents: </BotsDropDownLabel>
         <BotsDropDown onChange={(e) => selectBots(e)}>
           <option value='1' >1</option>
           <option value='2' >2</option>
