82505af3342cfcba66b067edd593e378b63fb5bd
nelliott82 <nikkoelliott@gmail.com>
2023-04-25 14:46:04 -0400

    Removed stray console log

--- client/src/helperFiles/socket.js
+++ client/src/helperFiles/socket.js
@@ -4,11 +4,9 @@ import { getCookie, setCookies } from './cookies.js';
 
 const URL = `${window.location.protocol}//${window.location.host}`;
 const socket = io(URL, { autoConnect: false });
-console.log('location host: ', window.location.host)
 
 socket.on('connect', () => {
   //!getCookie('uid') && setCookies([{ name: 'uid', value: socket.id }]);
-  console.log(window.location.host)
   setCookies([{ name: 'playerId', value: socket.id }], false);
 });
 
