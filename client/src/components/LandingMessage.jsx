f7ab1734cbab0d246573d16b1a7fdba6011f92cd
nelliott82 <nikkoelliott@gmail.com>
2023-05-11 15:19:07 -0400

    Improved LandingMessage to avoid flicker on first load

--- client/src/components/LandingMessage.jsx
+++ client/src/components/LandingMessage.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 
+const DisplayComponent = styled.div`
+  visibility: ${({ display }) => display ? 'visibile' : 'hidden'};
+`
+
 const LandingMessage = styled.div`
   z-index: 99;
   position: absolute;
@@ -82,6 +86,7 @@ const LandingMessageComponent = () => {
   const [top, setTop] = useState(0);
   const [buttonTop, setButtonTop] = useState(0);
   const [fontSize, setFontSize] = useState(6);
+  const [display, setDisplay] = useState(false);
 
   function handleReadyClick () {
     navigate('/select');
@@ -138,6 +143,7 @@ const LandingMessageComponent = () => {
 
   useEffect(() => {
     setScrolls();
+    setDisplay(true);
 
     const elementToObserve = msgScrollElement.current;
 
@@ -152,47 +158,49 @@ const LandingMessageComponent = () => {
 
   return (
     <>
-      <SkipButton onClick={handleReadyClick}>Skip</SkipButton>
-      <LandingMessage fontSize={fontSize} >
-        <MessageScroll ref={msgScrollElement}
-                      scroll={scroll}
-                      top={top}
-                      seconds={seconds} >
-          <p>
-            Welcome to the game of 99! This is a simple but fun card game
-            that is near and dear to my heart.
-          </p>
-          <p>
-            I made this site so I could play against friends from a distance and
-            also teach newcomers how to play. You can play against computers to practice
-            and learn, or, if you're feeling brave, against humans (humans not included).
-          </p>
-          <p>
-            The rules are fairly simple and are spelled out in more detail once
-            you're in the playing area, but, basically, you take turns playing a card.
-          </p>
-          <p>
-            Each card adds its own value to the round total. So, 3s add 3 points and 5s add
-            5 points. And Special Cards (4s, 10s, 9s, and Kings) do something different and
-            are crucial to avoiding losing. You can read more about the Special Cards in the
-            playing area.
-          </p>
-          <p>
-            The first player who can't keep the round total at, or, below 99 gets a strike
-            and a new round starts. Three strikes and you're out. Last player standing wins.
-          </p>
-          <p>
-            That's basically it! I hope you have fun!
-            -Nikko
-          </p>
-        </MessageScroll>
-        <ButtonContainer ref={buttonContainerElement}
-                        scroll={buttonScroll}
-                        top={buttonTop}
-                        seconds={buttonSeconds}>
-          <BeginButton ref={buttonElement} onClick={handleReadyClick}>Click here to begin</BeginButton>
-        </ButtonContainer>
-      </LandingMessage>
+      <DisplayComponent display={display} >
+        <SkipButton onClick={handleReadyClick}>Skip</SkipButton>
+        <LandingMessage fontSize={fontSize} >
+          <MessageScroll ref={msgScrollElement}
+                         scroll={scroll}
+                         top={top}
+                         seconds={seconds} >
+            <p>
+              Welcome to the game of 99! This is a simple but fun card game
+              that is near and dear to my heart.
+            </p>
+            <p>
+              I made this site so I could play against friends from a distance and
+              also teach newcomers how to play. You can play against computers to practice
+              and learn, or, if you're feeling brave, against humans (humans not included).
+            </p>
+            <p>
+              The rules are fairly simple and are spelled out in more detail once
+              you're in the playing area, but, basically, you take turns playing a card.
+            </p>
+            <p>
+              Each card adds its own value to the round total. So, 3s add 3 points and 5s add
+              5 points. And Special Cards (4s, 10s, 9s, and Kings) do something different and
+              are crucial to avoiding losing. You can read more about the Special Cards in the
+              playing area.
+            </p>
+            <p>
+              The first player who can't keep the round total at, or, below 99 gets a strike
+              and a new round starts. Three strikes and you're out. Last player standing wins.
+            </p>
+            <p>
+              That's basically it! I hope you have fun!
+              -Nikko
+            </p>
+          </MessageScroll>
+          <ButtonContainer ref={buttonContainerElement}
+                           scroll={buttonScroll}
+                           top={buttonTop}
+                           seconds={buttonSeconds}>
+            <BeginButton ref={buttonElement} onClick={handleReadyClick}>Click here to begin</BeginButton>
+          </ButtonContainer>
+        </LandingMessage>
+      </DisplayComponent>
     </>
     )
   }
